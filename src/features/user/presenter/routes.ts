9f5d4047e7dce9f578815bafca20e8a3750ebaae
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-09-19 01:37:10 -0300

    moved: create user logic to auth.create_account

--- src/features/user/presenter/routes.ts
+++ src/features/user/presenter/routes.ts
@@ -4,7 +4,6 @@ import { GetUsersController } from './controllers/get_users.controller';
 import { GetUsersUsecase } from '../data/usecases/get_users.usecase';
 import { GetUserController } from './controllers/get_user.controller';
 import { GetUserUsecase } from '../data/usecases/get_user.usecase';
-import { CreateUserController } from './controllers/create_user.controller';
 import { UpdateUserUsecase } from '../data/usecases/update_user.usecase';
 import { UpdateUserController } from './controllers/update_user.controller';
 import { RouterAdapter } from '../../../core/adapters/router.adapter';
@@ -29,16 +28,6 @@ userRoutes.get('/:id', async (req, res) => {
   const getUserUsecase = new GetUserUsecase(userRepository);
   const getUserController = new GetUserController(getUserUsecase);
   return getUserController.handleRequest(req, res);
-});
-
-// Create a new user
-userRoutes.post('/', authMiddleware(), async (req, res) => {
-  const createUserUsecase = new CreateUserUsecase(
-    userRepository,
-    encryptAdapter,
-  );
-  const createUserController = new CreateUserController(createUserUsecase);
-  return createUserController.handleRequest(req, res);
 });
 
 // Update an existing user

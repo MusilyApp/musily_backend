230671ff67b5eef919e3b3cbd5ae6b8f8d206a08
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-09-19 10:25:27 -0300

    added: i letter

--- src/features/library/domain/usecases/get_library_item.usecase.d.ts
+++ src/features/library/domain/usecases/get_library_item.usecase.d.ts
@@ -1,5 +1,5 @@
 import { LibraryItemEntity } from '../entities/library_item.entity';
 
-export interface GetLibraryItemUsecase {
+export interface IGetLibraryItemUsecase {
   exec<T>(id: string): Promise<LibraryItemEntity<T> | null>;
 }

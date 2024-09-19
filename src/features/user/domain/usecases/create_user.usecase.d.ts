94d7aeb7d96be5ea7bd2b82c49f2ca1962ea125a
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-09-19 01:36:05 -0300

    moved: create user logic to auth.create_account

--- src/features/user/domain/usecases/create_user.usecase.d.ts
+++
@@ -1,6 +0,0 @@
-import { ICreateUserDTO } from '../dtos/create_user_dto';
-import { IUserEntity } from '../entities/user.entity';
-
-export interface ICreateUserUsecase {
-  exec(userData: ICreateUserDTO): Promise<IUserEntity>;
-}

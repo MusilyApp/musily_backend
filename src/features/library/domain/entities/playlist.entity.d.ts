8f4d779a8255809bc05af10c6ba8ecbb14c36fc6
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-09-20 23:20:57 -0300

    changed track_count type to int

--- src/features/library/domain/entities/playlist.entity.d.ts
+++ src/features/library/domain/entities/playlist.entity.d.ts
@@ -3,6 +3,6 @@ import { ArtistEntity } from './artist.entity';
 export interface PlaylistEntity {
   id: string;
   title: string;
-  track_count: string;
+  track_count: number;
   artist: ArtistEntity | null;
 }

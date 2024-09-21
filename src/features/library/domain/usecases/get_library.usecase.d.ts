dd7a0585981f5858dc6cbe16f2140989e17831a6
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-09-20 23:23:17 -0300

    removed generic type

--- src/features/library/domain/usecases/get_library.usecase.d.ts
+++ src/features/library/domain/usecases/get_library.usecase.d.ts
@@ -1,5 +1,5 @@
 import { LibraryItemEntity } from '../entities/library_item.entity';
 
 export interface IGetLibraryUsecase {
-  exec<T>(userId: string): Promise<LibraryItemEntity<T>>;
+  exec(userId: string): Promise<LibraryItemEntity[]>;
 }
--- src/features/library/domain/usecases/get_library_item.usecase.d.ts
+++ src/features/library/domain/usecases/get_library_item.usecase.d.ts
@@ -1,5 +1,5 @@
 import { LibraryItemEntity } from '../entities/library_item.entity';
 
 export interface IGetLibraryItemUsecase {
-  exec<T>(id: string): Promise<LibraryItemEntity<T> | null>;
+  exec(id: string): Promise<LibraryItemEntity | null>;
 }

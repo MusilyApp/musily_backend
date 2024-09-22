15ee5a3889a310b047b1e5a38ea6c841600f0e52
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-09-22 15:12:01 -0300

    added userId property

--- src/features/library/domain/usecases/create_playlist.usecase.d.ts
+++ src/features/library/domain/usecases/create_playlist.usecase.d.ts
@@ -1,5 +1,5 @@
 import { PlaylistEntity } from '../entities/playlist.entity';
 
 export interface ICreatePlaylistUsecase {
-  exec(playlist: PlaylistEntity): Promise<PlaylistEntity>;
+  exec(playlist: PlaylistEntity, userId: string): Promise<PlaylistEntity>;
 }

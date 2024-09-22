7f87c412fdda8e96f83c603c4ec2471242121eda
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-09-22 15:08:12 -0300

    added userId property on createPlaylist method

--- src/features/library/domain/repositories/library.repository.d.ts
+++ src/features/library/domain/repositories/library.repository.d.ts
@@ -1,13 +1,17 @@
 import { LibraryItemEntity } from '../entities/library_item.entity';
 import { PlaylistEntity } from '../entities/playlist.entity';
 import { TrackEntity } from '../../../data_fetch/domain/entities/track.entity';
+import { UserTrackEntity } from '../entities/user_track.entity';
 
 export interface ILibraryRepository {
   getLibrary(userId: string): Promise<LibraryItemEntity[]>;
   getLibraryItem(id: string): Promise<LibraryItemEntity | null>;
-  addTrackToPlaylist(track: TrackEntity, playlistId: string): Promise<void>;
+  addTrackToPlaylist(track: UserTrackEntity): Promise<void>;
   removeTrackFromPlaylist(docummentId: string): Promise<void>;
-  createPlaylist(playlist: PlaylistEntity): Promise<PlaylistEntity>;
+  createPlaylist(
+    playlist: PlaylistEntity,
+    userId: string,
+  ): Promise<PlaylistEntity>;
   updatePlaylist(playlist: PlaylistEntity): Promise<PlaylistEntity>;
   deletePlaylist(playlistId: string): Promise<void>;
   addToLibrary(libraryItem: LibraryItemEntity): Promise<void>;

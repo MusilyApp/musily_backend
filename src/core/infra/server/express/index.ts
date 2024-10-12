26649d0e996b792aa513632d4b797b637e8af603
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-10-12 00:27:03 -0300

    added: library routes

--- src/core/infra/server/express/index.ts
+++ src/core/infra/server/express/index.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express, { Express } from 'express';
 import userRoutes from '../../../../features/user/presenter/routes';
 import authRoutes from '../../../../features/auth/presenter/routes';
+import libraryRoutes from '../../../../features/library/presenter/routes';
 
 const app: Express = express();
 app.use(express.json());
@@ -11,5 +12,6 @@ const port = process.env.PORT || 4000;
 
 app.use('/users', userRoutes.router);
 app.use('/auth', authRoutes.router);
+app.use('/library', libraryRoutes.router);
 
 app.listen(port, () => console.log(`App running at port ${port}`));
